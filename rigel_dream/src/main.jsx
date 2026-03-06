63dc16c7cca5372f47102b702f0a009c23cfaf50
arlix <beltranrojoalexs@gmail.com>
2026-03-05 23:54:27 -0600

    added page for products

--- rigel_dream/src/main.jsx
+++ rigel_dream/src/main.jsx
@@ -1,16 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { AuthProvider } from './context/AuthProvider'
+import {AuthProvider} from './context/AuthProvider'
 import ErrorBoundary from './components/ErrorBoundary'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <ErrorBoundary>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </ErrorBoundary>
-  </StrictMode>,
+    <StrictMode>
+        <ErrorBoundary>
+            <AuthProvider>
+                <App/>
+            </AuthProvider>
+        </ErrorBoundary>
+    </StrictMode>,
 )
