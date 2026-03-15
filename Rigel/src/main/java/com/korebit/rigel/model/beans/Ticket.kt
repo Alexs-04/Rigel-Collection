c5b75583ea9044b776f2264d80f6bfd75ffe8b5e
arlix <beltranrojoalexs@gmail.com>
2026-03-15 16:06:40 -0600

    added new entities and relations

---
+++ Rigel/src/main/java/com/korebit/rigel/model/beans/Ticket.kt
@@ -0,0 +1,32 @@
+package com.korebit.rigel.model.beans
+
+import com.korebit.rigel.model.extra.TicketDetail
+import jakarta.persistence.CascadeType
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.OneToMany
+import java.io.Serializable
+import java.math.BigDecimal
+import java.time.LocalDate
+
+@Entity(name = "tickets")
+class Ticket(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var folio: Long = 0,
+
+    @Column(nullable = false, length = 100)
+    var description: String = "",
+
+    @Column(nullable = false)
+    var dateAndTime: LocalDate = LocalDate.now(),
+
+    @Column(nullable = false, length = 20)
+    var totalAmount: BigDecimal = BigDecimal.ZERO,
+
+    @OneToMany(mappedBy = "ticket", cascade = [CascadeType.ALL], orphanRemoval = true)
+    var ticketDetails: MutableList<TicketDetail> = mutableListOf()
+) : Serializable
